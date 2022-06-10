88c8dcea992d8e400069eb4264c51edced743425
Iven <iven@Ivens-MacBook-Pro.local>
2022-06-10 18:28:50 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+body,html {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  overflow: hidden;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <iframe class="no-avatar" src="https://room.metaspace.place/?id=VGqA5XYbE2T5VVyErabW" frameborder="0"></iframe>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .no-avatar {
+    width: 100%;
+    height: 100vh;
+    display: block;
+  }
+</style>
